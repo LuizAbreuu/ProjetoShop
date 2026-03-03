7959774a3f027c0372cf3a544e286871f67ab9d7
luiz.abreu <luiz.abreu@NOT-SHP-014>
2026-03-03 16:46:16 -0300

    arrumando footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer(){
     const anoAtual = new Date().getFullYear();
 
     return(
-        <footer className=" p-6 bg-slate-950 text-center text-orange-500 py-3 fixed-bottom-0">
+        <footer className="bg-slate-950 text-center text-orange-500 py-3 mt-auto">
             <div className="container mx-auto text-center">
                 <p>&copy; {anoAtual} E-commerce/LuizShop. Todos os direitos reservados.</p>
             </div>
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import { Header } from "../header/Header";
 import { Footer } from "../footer/Footer";
 
-export function Layout(){
+export function Layout() {
     return (
-        <>
+        <div className="flex flex-col min-h-screen">
             <Header />
-            <Outlet />
+            <main className="flex-1">
+                <Outlet />
+            </main>
             <Footer />
-        </>
+        </div>
     )
 }
